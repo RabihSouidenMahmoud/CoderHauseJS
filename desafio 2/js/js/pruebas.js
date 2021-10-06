6ce42bf73d9541b5b1dc959ff3aabbc6f02d3a87
RabihSouidenMahmoud <86632928+RabihSouidenMahmoud@users.noreply.github.com>
2021-10-05 21:17:06 -0500

    Add files via upload

---
+++ desafio 2/js/js/pruebas.js
@@ -0,0 +1,27 @@
+// numero mayor a mil 
+let numeroMil = prompt("Ingrese un numero mayor a 1000");
+if (numeroMil  > 1000) {
+    alert ("el numero ingresado es mayor a 1000");
+}
+else {
+    alert("el numero ingresado es menor que 1000");
+}
+// Prueba hola
+let nombre = prompt("Escriba hola");
+if(nombre == "hola"){
+    alert("buenas");
+}
+else{
+    alert("Tenga modales"  );
+
+}
+// pruba rango numerico
+let numero = prompt("ingrese un numero");
+if((numero >10 ) &&  (numero <50)){
+    alert("su numero esta en el rango")
+}
+else  {
+    alert("su numero no esta en el rango entre 10 y 50")
+}
+
+
